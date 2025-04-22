8e8f530b15c631729bfecdd4f310fe666fd67106
SnigdhaSucharita <snigdhasucharita7@gmail.com>
2025-04-22 12:22:11 +0530

    fix: fixed the port

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const cors = require("cors");
-const { 
-    fetchMovies, 
-    createCuratedList, 
-    updateCuratedList, 
-    addToWatchlist, 
-    addToWishlist, 
-    addToCuratedList, 
-    addReviewAndRating, 
-    searchByGenreAndActor, 
-    sortListBy, 
-    getTopMovies
+const {
+  fetchMovies,
+  createCuratedList,
+  updateCuratedList,
+  addToWatchlist,
+  addToWishlist,
+  addToCuratedList,
+  addReviewAndRating,
+  searchByGenreAndActor,
+  sortListBy,
+  getTopMovies,
 } = require("./controller/controller");
 const { sequelize } = require("./models");
 
@@ -21,7 +21,7 @@ app.use(cors());
 
 app.get("/api/movies/search", fetchMovies);
 app.post("/api/movies/watchlist", addToWatchlist);
-app.post("/api/movies/wishlist", addToWishlist)
+app.post("/api/movies/wishlist", addToWishlist);
 app.post("/api/movies/curated-list", addToCuratedList);
 app.get("/api/movies/searchByGenreAndActor", searchByGenreAndActor);
 app.get("/api/movies/sort", sortListBy);
@@ -30,19 +30,18 @@ app.post("/api/movies/:movieId/reviews", addReviewAndRating);
 app.post("/api/curated-lists", createCuratedList);
 app.put("/api/curated-lists/:curatedListId", updateCuratedList);
 
-
 sequelize
-    .authenticate()
-    .then(() => {
-        console.log("Database connected.");
-    })
-    .catch((error) => {
-        console.error("Unable to connect to database.", error);
-    });
+  .authenticate()
+  .then(() => {
+    console.log("Database connected.");
+  })
+  .catch((error) => {
+    console.error("Unable to connect to database.", error);
+  });
 
-
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
 module.exports = app;
